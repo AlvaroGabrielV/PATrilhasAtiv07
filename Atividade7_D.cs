a5088d78bec622b1b3c9ceff0c6e793e12dc715a
AlvaroGabrielV <168542138+AlvaroGabrielV@users.noreply.github.com>
2025-02-11 08:55:21 -0300

    Atividade7_D.cs

---
+++ Atividade7_D.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace att01
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Random rand = new Random();
+			int[,] tabela = new int[10,10];
+			
+			for (int lin = 0; lin < 10; lin++) {
+				Console.Write("\n\r ");	
+					
+				for (int col = 0; col < 10; col++) {
+					tabela [lin,col] = rand.Next(0 ,9);
+					if (lin == col) {
+						Console.Write(" " + " ");
+					}
+					else {
+						Console.Write(tabela[lin, col] + " ");
+					}
+					
+				}
+			}
+
+			Console.WriteLine("\n\rAperte qualquer tecla para continuar...");
+			Console.ReadKey(true);
+		}
+	}
+}
